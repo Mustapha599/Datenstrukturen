4342303027c232ecb74ca1167cb940acc71d81c9
Mustapha599 <belaoun.mustapha@outlook.de>
2025-01-28 21:23:47 +0100

    Update Knoten.java

--- Knoten.java
+++ Knoten.java
@@ -1,21 +1,24 @@
 public class Knoten<T> {
-    public T zeigeAuf;                             //Speichert das Objekt auf welches der Knoten zeigt
-    private Knoten nachfolger;                      //Speichert den nachfolgenden Knoten
+    // Generische Klasse, die einen Knoten repräsentiert. Jeder Knoten speichert ein Element und zeigt auf einen Nachfolger.
 
+    public T zeigeAuf; // Speichert das Objekt, auf das dieser Knoten zeigt (Dateninhalt des Knotens).
+    private Knoten nachfolger; // Verweist auf den nächsten Knoten in der Kette.
+
+    // Konstruktor: Initialisiert einen neuen Knoten mit dem angegebenen Objekt.
     public Knoten(T pZeigeAuf){
-
-        zeigeAuf = pZeigeAuf;
+        zeigeAuf = pZeigeAuf; // Setzt das übergebene Objekt als Dateninhalt.
     }
+
+    
     public Knoten getNachfolger(){
-        return nachfolger;
+        return nachfolger; // Gibt den Verweis auf den nächsten Knoten zurück.
     }
+
+    .
     public void setNachfolger(Knoten pNachfolger){
-
-        this.nachfolger = pNachfolger;
+        this.nachfolger = pNachfolger; // Verbindet diesen Knoten mit dem übergebenen Nachfolger.
     }
+
+    // Getter-Methode: Liefert das Objekt zurück, auf das dieser Knoten zeigt.
     public T getZeigeAuf(){
-
-        return zeigeAuf;
-    }
-}
-
+        return zeigeAuf; // Gibt den gespeicherten Dateninhalt 
