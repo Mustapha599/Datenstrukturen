ac93015de1a66da39f6dc357d6776d85b189e201
belaoumu <belaoun.mustapha@outlook.de>
2025-01-31 10:40:07 +0100

    Finale

--- stack.java
+++ stack.java
@@ -1,10 +1,24 @@
 public class stack <E> {
+    private Knoten erster;
     public E getContent(){
 
         return null;
     }
-public void add(E content){
+public void einfuegen(E content){
+    Knoten hilfe = new Knoten(content);
+    if (erster == null) {
+        // Wenn die Warteschlange leer ist, wird der Kunde der erste
+        erster = hilfe;
+    } else {
+        // Sonst zum letzten Kunden führen und den neuen anhängen
+        Knoten aktueller = erster;
 
+        while (aktueller.getNachfolger() != null) {  // Schleife läuft so lange, wie es einen Nachfolger gibt
+            aktueller = aktueller.getNachfolger(); // Nachfolger wird zu aktueller
+        }
+        aktueller.setNachfolger(hilfe); // Mit aktueller.setNachfolger(pKnoten) wird der neue Knoten (pKnoten) als Nachfolger des letzten Knoten gesetzt.
+    }
+}
 }
 public void remove(){
 
