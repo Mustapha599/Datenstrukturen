4e75d15115c7114836ec07b0fa544c34c7e37a13
belaoumu <belaoun.mustapha@outlook.de>
2025-02-05 10:36:49 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	stack.java

--- list.java
+++ list.java
@@ -1,58 +1,69 @@
 
-public class list <A>{
-    public A  first;
-    public A last;
-    public A current;
-    private Node erster;
-    public list(){
+public class list <A> {
 
+    public Node last;
+    public Node current;
+    public Node first;
+
+    public list() {
+        first = null;
+        current = null;
+        last = null;
     }
-//Einfuegen hinten
-public void append(A pC){
-    Node hilfe = new Knoten(A pC);
-    if (first == null) {
-        // Wenn die Warteschlange leer ist, wird der Kunde der erste
-        erster = (Node) hilfe;
-    } else {
-        // Sonst zum letzten Kunden führen und den neuen anhängen
-        Node aktueller = erster;
 
-        while (aktueller.getNachfolger() != null) {  // Schleife läuft so lange, wie es einen Nachfolger gibt
-            aktueller = aktueller.getNachfolger(); // Nachfolger wird zu aktueller
+    //Einfuegen hinten
+    public void append(A pContent) {
+        Node hilfe = new Node(pContent);
+        if (first == null) {
+            // Wenn die Warteschlange leer ist, wird der Kunde der erste
+            first = (Node) hilfe;
+        } else {
+            // Sonst zum letzten Kunden führen und den neuen anhängen
+            Node aktueller = first;
+
+            while (aktueller.getNachfolger() != null) {  // Schleife läuft so lange, wie es einen Nachfolger gibt
+                aktueller = aktueller.getNachfolger(); // Nachfolger wird zu aktueller
+            }
+            aktueller.setNachfolger(hilfe); // Mit aktueller.setNachfolger(pKnoten) wird der neue Knoten (pKnoten) als Nachfolger des letzten Knoten gesetzt.
         }
-        aktueller.setNachfolger(hilfe); // Mit aktueller.setNachfolger(pKnoten) wird der neue Knoten (pKnoten) als Nachfolger des letzten Knoten gesetzt.
     }
-}
-}
-public void next(){
 
-}
-//Einfuegen zwischen
-public void insert(A pC){
-
-}
-
-public void toFirst(){
-
-}
-public void current(){
-
-}
-public void toLast(){
-
-}
-public void remove(){
+    public void next() {
 
     }
-public boolean hasAccess(){
 
-    return false;
-}
-public A getContent(){
+    //Einfuegen zwischen
+    public void insert(A) {
 
-    return null;
-}
-public void setContent(A pC){
+    }
 
-}
+    public void toFirst() {
+        if (first != null) {
+            first = hilfe;
+        } else {
+            if (first == null) {
+                first =
+            }
+        }
+        public void current () {
+
+        }
+        public void toLast () {
+
+        }
+        public void remove () {
+
+        }
+        public boolean hasAccess () {
+
+            return false;
+        }
+        public A getContent () {
+
+            return null;
+        }
+        public void setContent (A pC){
+
+        }
+    }
 }
