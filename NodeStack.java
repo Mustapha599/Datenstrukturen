fc6a098f020c95ee5b36ee6033bc11b05cf88540
Mustapha599 <belaoun.mustapha@outlook.de>
2025-02-06 21:38:54 +0000

    Update NodeStack.java

--- NodeStack.java
+++ NodeStack.java
@@ -16,3 +16,33 @@ public void setNextNode(NodeStack nextNode){
 
     }
 }
+-----------------
+    private E content;
+    private Node<E> nextNode;
+
+    // Konstruktor
+    public Node(E pContent) {
+        this.content = pContent;
+        this.nextNode = null;
+    }
+
+    // Getter für Content
+    public E getContent() {
+        return content;
+    }
+
+    // Setter für Content
+    public void setContent(E pContent) {
+        this.content = pContent;
+    }
+
+    // Getter für nächsten Node
+    public Node<E> getNextNode() {
+        return nextNode;
+    }
+
+    // Setter für nächsten Node
+    public void setNextNode(Node<E> nextNode) {
+        this.nextNode = nextNode;
+    }
+}
