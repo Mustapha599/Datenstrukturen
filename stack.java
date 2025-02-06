82a2852b47b006183f1b0fb2f9ce380a9f24a150
Mustapha599 <belaoun.mustapha@outlook.de>
2025-02-06 21:48:06 +0000

    Update stack.java

--- stack.java
+++ stack.java
@@ -27,3 +27,18 @@ public boolean isEmpty(){
     return false;
 }
 }
+--------------
+    public void add(E content) {
+        Node<E> newNode = new Node<>(content);// Erstellt einen neuen Knoten, der auf das aktuelle oberste Element zeigt
+        newNode.setNextNode(erster);
+        erster = newNode;
+
+public E remove() {
+        if (erster == null) {// Wenn der Stapel leer ist, wird null zurückgegebe
+            return null;
+        }
+        E content = erster.getContent();// Inhalt des obersten Elements
+        erster = erster.getNextNode();// Setzt das nächste Element als oberstes
+        return content; // Gibt den entfernten Inhalt zurück
+
+    
